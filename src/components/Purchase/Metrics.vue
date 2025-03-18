ebffba00b3a5cf400b246053b47ed11040a94cb0
Abdul Samad Kirmani <abdul.samadkirmani.samad63@gmail.com>
2025-03-18 16:28:56 +0530

    Starting purchase section

---
+++ src/components/Purchase/Metrics.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-6">
+        <div class="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+            <div class="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+                <CashIcon/>
+            </div>
+
+            <div class="flex items-end justify-between mt-5">
+                <div>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Total Purchases</span>
+                    <h4 class="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">1,93,400</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+            <div class="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+                <RupeeIcon/>
+            </div>
+
+            <div class="flex items-end justify-between mt-5">
+                <div>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Paid</span>
+                    <h4 class="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">5</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+            <div class="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+                <RupeeIcon/>
+            </div>
+
+            <div class="flex items-end justify-between mt-5">
+                <div>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">unpaid</span>
+                    <h4 class="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">5</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import CashIcon from '@/icons/CashIcon.vue';
+import RupeeIcon from '@/icons/RupeeIcon.vue';
+
+export default {
+    components: {
+        CashIcon,
+        RupeeIcon,
+    },
+    name: 'Metrics'
+}
+
+</script>
